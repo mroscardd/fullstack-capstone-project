0c173e2de951918e857645a5b4e9ddbd5c3bb763
mroscardd <mroscardd@gmail.com>
2026-09-09 18:59:44 +0200

    search

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -24,7 +24,7 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 const giftRoutes = require('./routes/giftRoutes')
-app.use('/api/gifts', giftRoutes)
+const searchRoutes = require('./routes/searchRoutes')
 
 
 const pinoHttp = require('pino-http');
@@ -32,12 +32,9 @@ const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
-// Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
 
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/gifts', giftRoutes)
+app.use('/api/search', searchRoutes)
 
 
 // Global Error Handler
