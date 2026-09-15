239eab3ddef02cf059dd43009c67134902aaa012
mroscardd <mroscardd@gmail.com>
2026-09-15 12:03:56 +0200

    login front

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,5 +1,9 @@
 import  './LoginPage.css'
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { urlConfig } from '../../config'
+import { useAppContext } from '../../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
+
 
 function LoginPage() {
     const [formLogin, setFormLogin] = useState(
@@ -9,16 +13,61 @@ function LoginPage() {
         }
     )
 
+    const [incorrect, setIncorrect] = useState('')
+    const navigate = useNavigate()
+    const {setIsLoggedIn} = useAppContext()
+    const bearerToken = sessionStorage.getItem('bearer-token')
+
+    useEffect(() => {
+        if (sessionStorage.getItem('auth-token')) {
+            navigate('/app')
+        }
+        }, [navigate])
 
     const handleChange = (propiedad, e) => {
         const element = e.target.value
         setFormLogin({...formLogin, [propiedad]: element})
     }    
 
-    const handleRegister = (e) => {
-        e.preventDefault()
-        console.log("Dentro de handleRegister")
+    const handleLogin = async () => {
+	try{
+		//first task
+      const response = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
+		    method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': bearerToken ? `Bearer ${bearerToken}` : ''
+                },
+            body: JSON.stringify({    
+                email: formLogin.email,
+                password: formLogin.password,
+                })
+
+	        })
+        const json = await response.json();
+        console.log('Json',json);
+        if (json.authtoken) {
+		
+          sessionStorage.setItem('auth-token', json.authtoken);
+          sessionStorage.setItem('name', json.userName);
+          sessionStorage.setItem('email', json.userEmail);
+			
+          setIsLoggedIn(true);
+	
+	      navigate('/app');
+        } else {
+			//Step 2: Task 5
+          document.getElementById("email").value="";
+          document.getElementById("password").value="";
+          setIncorrect("Wrong password. Try again.");
+          setTimeout(() => {
+            setIncorrect("");
+          }, 2000);
+        }    
+	  }catch (e) {
+        console.log("Error fetching details: " + e.message);
     }
+}
    
     return (
         <div className="environment">
@@ -44,10 +93,12 @@ function LoginPage() {
                         onChange={(e) => handleChange("password", e)}
                         value={formLogin.password}
                     />
-                    <button className="btn-primary btnForm" onSubmit={handleRegister}>Register</button>
+                    <button className="btn-primary btnForm" onSubmit={handleLogin}>Register</button>
                     <p className="mt-4 text-center">
                         ¿Ya eres miembro? <a href="/app/login" className="text-primary">Iniciar sesión</a>
                     </p>
+                    <span style={{color:'red',height:'.5cm',display:'block',fontStyle:'italic',fontSize:'12px'}}>{incorrect}</span>
+
                 </form>
             </div>
         </div>    
